2566cc4e44918ca8643ebf8dcac7197f20517501
ffalt <ffalty@gmail.com>
2018-02-21 15:46:29 +0100

    export csv bulk download

--- bin/save_downloads.js
+++ bin/save_downloads.js
@@ -16,8 +16,14 @@ const Store = require('../lib/store.js');
 const config = require('../config.js');
 const console = status.console();
 const Utils = require('../lib/utils');
+const CSV = require('../lib/csv');
+const Library = require('../lib/library.js');
 
 const now = (new Date()).valueOf();
+
+const library = new Library(config);
+const csv = new CSV(library);
+
 
 let currentCountry = '-';
 let status_items = status.addItem('items');
@@ -88,16 +94,22 @@ let dump = (country, cb) => {
 	let file_json_compress = compressStream(file_json_stream);
 	file_json_compress.write('[');
 
+	let file_csv = {filename: filename + '.csv.gz', size: 0};
+	let file_csv_stream = downloadFolderFileStream(file_csv.filename);
+	let file_csv_compress = compressStream(file_csv_stream);
+	file_csv_compress.write(csv.header());
+
 	let totalItems = 0;
 
 	file_ndjson_stream.on('close', () => {
 		setTimeout(() => {
 			file_json.size = fs.statSync(path.join(downloadsFolder, file_json.filename)).size;
 			file_ndjson.size = fs.statSync(path.join(downloadsFolder, file_ndjson.filename)).size;
-			let result = {country: countryId, count: totalItems, lastUpdate: now, formats: {json: file_json, ndjson: file_ndjson}};
+			file_csv.size = fs.statSync(path.join(downloadsFolder, file_csv.filename)).size;
+			let result = {country: countryId, count: totalItems, lastUpdate: now, formats: {json: file_json, ndjson: file_ndjson, csv: file_csv}};
 			results.push(result);
 			cb();
-		}, 100);
+		}, 1000);
 	});
 
 	streamItems(countryId,
@@ -105,9 +117,10 @@ let dump = (country, cb) => {
 			if (totalItems !== 0) {
 				file_json_compress.write(',');
 			}
-			items.forEach(item => {
+			items.forEach((item, index) => {
 				Utils.cleanOtFields(item._source);
 				file_ndjson_serialize.write(item._source);
+				file_csv_compress.write(csv.transform(item._source, pos + index));
 			});
 			file_json_compress.write(items.map(item => {
 				return JSON.stringify(item._source);
@@ -123,6 +136,7 @@ let dump = (country, cb) => {
 			} else {
 				file_json_compress.write(']');
 			}
+			file_csv_compress.end();
 			file_json_compress.end();
 			file_ndjson_serialize.end();
 			file_ndjson_compress.end();
